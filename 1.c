0569556ad62c948468a51afc947ceccdf46e984a
cjp <chenjp1@dcms.com.cn>
2011-06-24 16:40:50 +0800

    20110624 cjp create

---
+++ 1.c
@@ -0,0 +1,14 @@
+/* cjp create */
+int main(int arg,char * arv[])
+{ float s,i,j;
+  printf("c language");
+  scanf("%f",&i);
+  scanf("%f",&j);
+  s=sum(i,j);
+  return 0;
+}
+float sum(float a,float b)
+{ float s;
+  s=a+b;
+  return s
+}
